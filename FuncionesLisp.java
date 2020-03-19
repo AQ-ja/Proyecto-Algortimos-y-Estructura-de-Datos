6c73273b4bd92e86ceb641f6f8cd0350309a44e2
Orlando Cabrera <orlando.osberto@gmail.com>
2020-03-18 21:15:06 -0600

    Arregle algunas cosas para que coincidan entre si

--- FuncionesLisp.java
+++ FuncionesLisp.java
@@ -9,13 +9,16 @@ import java.util.HashMap;
  * @author Orlando
  *
  */
-public class FuncionesLisp {
+public class FuncionesLisp  {
 	private HashMap<String,String> funciones = new HashMap<String,String>();
+	private HashMap<String,String> variables = new HashMap<String,String>();
+	private String FuncionesLisp;
+	private String Condicionales;
 	private ArrayList<String> funK = new ArrayList<String> ();
 	private ArrayList<String> funV = new ArrayList<String> ();
-	private ArrayList<String> variables = new ArrayList<String> ();
+	/*
 	public FuncionesLisp () {
-		funK.add("defunK");
+		funK.add("defun");
 		funK.add("cond");
 		funK.add("setq");
 		funK.add("atom");
@@ -35,7 +38,7 @@ public class FuncionesLisp {
 		for (int i =0;i<funK.size();i++) {
 			funciones.put(funK.remove(i), funV.remove(i));
 		}
-	}
+	}*/
 	
 	public String setfunciones(String funKcion,String cuerpo) {
 		for (HashMap.Entry<String, String> entry:funciones.entrySet())
@@ -47,37 +50,12 @@ public class FuncionesLisp {
 		funciones.put(funKcion, cuerpo);
 		return "funcion agregada";
 	}
-	
-	//Intento de equivalencias entre java y lisp
-	// este seria el mas facil xd. 
-	public boolean equals(E valorA, E valorB){ //Tenemos 2 genericos y si son iguales regresa true, sino false
-        return  valorA.equals(valorB);
-    }
-	
-
-	public Boolean mayorMenor(int valorA, int valorB ){
-        if(valorA > valorB){
-            return true;
-        }else{
-            return false;
-        }
-	}
-
-	public Boolean menorMayor(int valorA, int valorB ){
-        if(valorA < valorB){
-            return true;
-        }else{
-            return false;
-        }
-    }
-	
-	
+	@SuppressWarnings("null")
 	public ArrayList<String> list(String val){
 		ArrayList<String> resultado = null;
 		resultado.add(val);
         return resultado;
     }
-	
 	public <E> boolean atom(E val){
 		if (val instanceof ArrayList) {
 			return false;
@@ -87,8 +65,10 @@ public class FuncionesLisp {
 		
         
     }
-	public void setq(String cadena) {
-		variables.add(cadena);
+	public void setq(String key, String cadena) {
+		variables.put(key, cadena);
 	}
+	
+	
+	
 }
-
